bffaed5e275a87596813ad5cbb652f177d4c98c4
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-02-05 14:38:09 +0100

    Add click events
    Services navigation
    Service article drop-downs
    Wing icons rotation

---
+++ js/services.js
@@ -0,0 +1,41 @@
+/******************/
+/* SERVICES NAV */
+/*****************/
+
+$("button.services-navigation").click(function () {
+  $("div.services-nav").toggle();
+});
+
+
+/******************/
+/* DROP-DOWN'S */
+/*****************/
+
+$("button.drop-down-icon").click(function () {
+
+  if ($(this).siblings("div.service-article").hasClass("hidden")) {
+    $(this).siblings("div.service-article").slideDown().removeClass("hidden");
+  } else {
+    $(this).siblings("div.service-article").slideUp().addClass("hidden");
+  }
+
+})
+
+
+/******************/
+/* ROTATE WINGS */
+/*****************/
+
+const wingsArr = $("img.wings");
+
+for (let i = 0; i <= wingsArr.length; i++) {
+  let wingsIndex = $(wingsArr[i]);
+  // wingsIndex is NaN
+  let rotation = i * 30;
+  wingsIndex.css({
+    "transform": `rotate(${rotation}deg)`
+  });
+  console.log(rotation);
+
+}
+
