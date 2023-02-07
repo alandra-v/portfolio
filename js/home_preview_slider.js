f651879ac6c0f568b91bb422c62873686642b7ba
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-02-07 15:59:30 +0100

    Project preview slider
    3 buttons: next slide, previous slide, autoplay

---
+++ js/home_preview_slider.js
@@ -0,0 +1,132 @@
+/*************/
+/* SLIDER */
+/*************/
+
+/* VARIABLES */
+
+// get li (slideWidth) width 
+let projectWidth = $("ul.previews-ul>li").width();
+let gapWidth = calcGapWidth();
+let slideWidth = projectWidth + gapWidth;
+
+
+// start with the 1. slide
+$("ul.previews-ul > li").last().prependTo(".previews-ul");
+
+
+
+/* FUNCTIONS */
+
+// calculate gapWidth
+function calcGapWidth() {
+
+  let positionProject1 = $("ul.previews-ul li:nth-child(1)").offset().left + $("ul.previews-ul li:nth-child(1)").width();
+  let positionProject2 = $("ul.previews-ul li:nth-child(2)").offset().left;
+  let space = positionProject2 - positionProject1;
+  // console.log(space);
+  return space;
+
+};
+// console.log($("ul.previews-ul:nth-child(1)").offset());
+
+
+// recalculate li width when window is rezized (responsive)
+$(window).resize(function () {
+  projectWidth = $("ul.previews-ul>li").width();
+  gapWidth = calcGapWidth();
+  let slideWidth = projectWidth + gapWidth;
+});
+
+
+function moveLeft() {
+  $(".previews-ul").animate({
+    "left": `-=${slideWidth}px`
+  },
+    500,
+    function () {
+      $(".previews-ul").css("left", "");
+      $("ul.previews-ul>li").first().appendTo(".previews-ul");
+    });
+};
+
+function moveRight() {
+  $(".previews-ul").animate({
+    "left": `+=${slideWidth}px`
+  },
+    500,
+    function () {
+      $(".previews-ul").css("left", "");
+      $("ul.previews-ul>li").last().prependTo(".previews-ul");
+    });
+};
+
+
+/* EVENTLISTENERS */
+
+$(".r-a").click(function () {
+  moveLeft();
+  clearInterval(autoplayTimer);
+  // console.log("interval cleared");
+  removeTimer();
+});
+
+$(".l-a").click(function () {
+  moveRight();
+  clearInterval(autoplayTimer);
+  removeTimer();
+});
+
+
+
+/*************/
+/* AUTOPLAY */
+/*************/
+
+/* VARIABLES */
+
+let toggle = $(".toggle");
+let toggleText = $(".toggle-text");
+let autoplayTimer;
+
+/* EVENTLISTENER */
+
+toggle.click(function (e) {
+  toggleAnimated(e);
+  timer(e);
+})
+
+/* FUNCTIONS */
+
+// animation function
+function toggleAnimated() {
+
+  toggle.toggleClass("active");
+
+  if (toggle.hasClass("active")) {
+    toggleText.text = "ON";
+  } else {
+    toggleText.text = "OFF";
+  };
+};
+
+// timer function
+function timer() {
+  if (toggle.hasClass("active")) {
+    autoplayTimer = setInterval(function () {
+      moveLeft();
+      // console.log("slide");
+      // console.log(autoplayTimer);
+    }, 3000);
+  } else {
+    clearInterval(autoplayTimer);
+  };
+}
+
+// clearInterval on left/right buttons
+function removeTimer() {
+  if (toggle.hasClass("active")) {
+    toggle.removeClass("active");
+    // console.log("class active removed");
+  }
+}
+
