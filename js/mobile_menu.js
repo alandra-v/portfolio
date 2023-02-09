057d35b2cdb1f819ccc5c2d05d33a9617ede4640
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-02-09 14:41:19 +0100

    Adjust mobile menu for subpages with a background image
    white nav icons when closed
    black nav icons when open

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -14,14 +14,39 @@ burgerNav.click(function () {
 
   menuOverlay.toggle();
 
+  // disable scroll when menu is open
   if (this.classList.contains("open")) {
     // disable scroll
     $("body").css({ "overflow": "hidden" });
+    // ensure close button is black
+    $("span.burger").css("background", "#000");
+    $(".avd-logo-svg").attr("src", "icons/avd_logo_black.svg");
   } else {
     // enable scroll
     $("body").css({ "overflow": "auto" });
+    if ($("div.background-img").length) {
+      $("span.burger").css("background", "#fff");
+      $(".avd-logo-svg").attr("src", "icons/avd_logo.svg");
+
+    } else {
+      $("span.burger").css("background", "#000");
+      $(".avd-logo-svg").attr("src", "icons/avd_logo_black.svg");
+    }
   }
+
+  // replace logo with black logo when menu is open
+
+  // $("span.burger").toggle("close");
+
 });
+
+// check if document has a background image 
+// if so change nav icons to white
+if ($("div.background-img").length) {
+  $("span.burger").css("background", "#fff");
+  $(".avd-logo-svg").attr("src", "icons/avd_logo.svg");
+}
+
 
 // hide mobile menu when window size is changed
 $(window).on("resize", function (e) {
