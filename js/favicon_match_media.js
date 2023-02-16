6b4f583ca1ab00d2b14b1614f15bb05b89340aa3
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-02-16 15:53:20 +0100

    Replace single quotes by double quotes

--- js/favicon_match_media.js
+++ js/favicon_match_media.js
@@ -1,19 +1,19 @@
 // css media matcher
-let matchResult = window.matchMedia('(prefers-color-scheme: dark)');
+let matchResult = window.matchMedia("(prefers-color-scheme: dark)");
 
 
 // add / remove the elements from html head
-lightSchemeIcon = document.querySelector('link#light-scheme-icon');
-darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
+lightSchemeIcon = document.querySelector("link#light-scheme-icon");
+darkSchemeIcon = document.querySelector("link#dark-scheme-icon");
 
 function onUpdate() {
   if (matchResult.matches) {
     lightSchemeIcon.remove();
     document.head.append(darkSchemeIcon);
-    console.log("true");
+    // console.log("true");
   } else {
     document.head.append(lightSchemeIcon);
     darkSchemeIcon.remove();
-    console.log("false");
+    // console.log("false");
   }
 }
