4c52596a69815c20deb4708d3bc186c78380449c
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-02-17 10:01:56 +0100

    Reduce code duplication, rename file

--- js/mobile_navigation.js
+++ js/mobile_navigation.js
@@ -7,14 +7,16 @@ let scrolling = true;
 
 /* FUNCTIONS */
 
-// burger menu transition
+$(document).ready(whiteIconsNecessity);
+
+// mobile navigation transition
 burgerNav.click(function () {
 
   $(this).toggleClass("open");
 
   menuOverlay.toggle();
 
-  // disable scroll when menu is open
+  // disable scroll when navigation is open
   if (this.classList.contains("open")) {
     // disable scroll
     $("body").css({ "overflow": "hidden" });
@@ -24,44 +26,32 @@ burgerNav.click(function () {
   } else {
     // enable scroll
     $("body").css({ "overflow": "auto" });
-    // if there is a background image use white icons
-    if ($("div.background-img").length) {
-      $("span.burger").css("background", "#fff");
-      $(".avd-logo-svg").attr("src", "assets/icons/avd_logo.svg");
-
-    } else {
-      $("span.burger").css("background", "#000");
-      $(".avd-logo-svg").attr("src", "assets/icons/avd_logo_black.svg");
-    }
+    whiteIconsNecessity();
   }
 
 });
 
-// check if document has a background image 
-// if so change nav icons to white
-if ($("div.background-img").length) {
-  $("span.burger").css("background", "#fff");
-  $(".avd-logo-svg").attr("src", "assets/icons/avd_logo.svg");
-}
-
-
-// hide mobile menu when window size is changed
+// hide mobile navigation when window size is changed
 $(window).on("resize", function () {
   clearTimeout(resizeTimer);
   resizeTimer = setTimeout(function () {
     if ($(window).width() > 1000) {
       menuOverlay.hide();
+      burgerNav.removeClass("open");
     }
-  }, 100);
+  }, 50);
 
-  // if there is a background image use white icons
+  whiteIconsNecessity();
+});
+
+// if there is a background image use white icons
+function whiteIconsNecessity() {
   if ($("div.background-img").length) {
     $("span.burger").css("background", "#fff");
     $(".avd-logo-svg").attr("src", "assets/icons/avd_logo.svg");
-
   } else {
     $("span.burger").css("background", "#000");
     $(".avd-logo-svg").attr("src", "assets/icons/avd_logo_black.svg");
   }
-});
+}
 
