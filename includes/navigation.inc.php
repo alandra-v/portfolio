744c2d1223ec979f8a2afc6d81a42a9d67a45a7c
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-05-11 07:09:56 +0200

    Change nav icon color on certain pages

--- includes/navigation.inc.php
+++ includes/navigation.inc.php
@@ -17,27 +17,38 @@
       <?php
       }
       ?>
-      <ul class="small-menu">
-        <?php
-        foreach ($legalsArray as $main) {
-        ?>
-          <li><a href="<?php echo $main['link']; ?>" <?php if ($currentPage == $main['link']) {
-                                                        echo 'active';
-                                                      } ?>><?php echo $main['title']; ?></a></li>
-        <?php
-        }
-        ?>
-      </ul>
+      <li>
+        <ul class="small-menu">
+          <?php
+          foreach ($legalsArray as $main) {
+          ?>
+            <li><a href="<?php echo $main['link']; ?>" <?php if ($currentPage == $main['link']) {
+                                                          echo 'active';
+                                                        } ?>><?php echo $main['title']; ?></a></li>
+          <?php
+          }
+          ?>
+        </ul>
       </li>
       <li><button class="dark-mode-switch" aria-label="Switch to dark mode"><img src="assets/icons/dark_mode_switch.svg" width="50" height="50" alt=""></button></li>
       <li><button class="language-switch-menu" aria-label="Change the language to german">EN &#10072; DE</button>
       </li>
     </ul>
+    </li>
   </div>
   <ul class="nav-switches">
-    <li><button class="theme-switch" aria-label="Switch to dark mode"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41.81 58.22">
-          <path stroke="#000" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" d="M40.81 29.11c0 15.53-12.58 28.11-28.11 28.11-3.46 0-6.77-.63-9.84-1.77 10.68-3.99 18.27-14.27 18.27-26.34S13.55 6.76 2.86 2.77C5.92 1.63 9.24 1 12.7 1c15.53 0 28.11 12.58 28.11 28.11Z" />
-        </svg></button></li>
+    <li><button class="theme-switch" aria-label="Switch to dark mode">
+        <?php
+        if ($currentPage == 'index.php' || $currentPage == 'about.php') {
+          echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41.81 58.22">
+        <path stroke="#fff" fill="#fff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" d="M40.81 29.11c0 15.53-12.58 28.11-28.11 28.11-3.46 0-6.77-.63-9.84-1.77 10.68-3.99 18.27-14.27 18.27-26.34S13.55 6.76 2.86 2.77C5.92 1.63 9.24 1 12.7 1c15.53 0 28.11 12.58 28.11 28.11Z" />
+        </svg>';
+        } else {
+          echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41.81 58.22">
+        <path stroke="#000" fill="#000" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" d="M40.81 29.11c0 15.53-12.58 28.11-28.11 28.11-3.46 0-6.77-.63-9.84-1.77 10.68-3.99 18.27-14.27 18.27-26.34S13.55 6.76 2.86 2.77C5.92 1.63 9.24 1 12.7 1c15.53 0 28.11 12.58 28.11 28.11Z" />
+        </svg>';
+        }
+        ?></button></li>
     <li><button class="language-switch-header" aria-label="Change the language to german">EN &#10072; DE</button>
     </li>
   </ul>
