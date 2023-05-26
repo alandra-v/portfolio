db15a0f025208a7a1e5b37cf68e00cca58a33576
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-05-26 07:12:17 +0200

    Add PHPMailer

---
+++ admin/phpmailer/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('SMTP_HOST', 'smtpauths.bluewin.ch');
+define('SMTP_USER', 'selina.villalaz@bluewin.ch');
+define('SMTP_PASSWORD', 'esischnoetig19');
+define('SMTP_PORT', 465);
+define('SMTP_SECURE', 'SSL');
+define('SMTP_AUTH', 'true');
