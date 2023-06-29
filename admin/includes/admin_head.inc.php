777215a924c484fb1ff0e1ad15f0db74db15fce5
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-06-29 16:22:43 +0200

    Update head with links and scripts

--- admin/includes/admin_head.inc.php
+++ admin/includes/admin_head.inc.php
@@ -15,39 +15,41 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+  <!-- stylesheets & scripts-->
+  <link rel="stylesheet" href="<?= BASE_URL . 'css/reset.css' ?>">
+  <link rel="stylesheet" href="<?= BASE_URL . 'admin/css/variables.css' ?>">
+  <script src="<?= BASE_URL . 'js/favicon_match_media.js' ?>" defer></script>
+
+
   <!-- dynamic stylesheets & scripts-->
   <?php
 
   switch ($currentPage) {
     case 'login.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/admin_header.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/footer.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/login.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/login_validation.js" defer></script>';
       break;
 
     case 'password_forgotten.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/admin_header.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/footer.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/password_forgotten.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/password_forgotten.js" defer></script>';
       break;
 
     case 'password_reset.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/admin_header.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/footer.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/password_forgotten.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/password_reset.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
-      '<script src="' . BASE_URL . 'admin/js/password_reset.js" defer></script>';
+      '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/password_requirements.css">',
+      '<script src="' . BASE_URL . 'admin/js/password_reset.js" defer></script>',
+      '<script src="' . BASE_URL . 'admin/js/password_functions.js" defer></script>';
       break;
 
 
@@ -55,11 +57,9 @@
 
     case 'index.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/style.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
       '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>';
@@ -67,23 +67,22 @@
 
     case 'user_registration.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/admin_header.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/footer.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/registration.css">',
-      '<script src="' . BASE_URL . 'admin/js/registration_validation.js" defer></script>';
+      '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/password_requirements.css">',
+      '<script src="' . BASE_URL . 'admin/js/registration_validation.js" defer></script>',
+      '<script src="' . BASE_URL . 'admin/js/password_functions.js" defer></script>';
       break;
 
     case 'account_settings.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/account_settings.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
+      '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/confirmation_modal.css">',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/confirmation.js" defer></script>';
@@ -92,11 +91,10 @@
 
     case 'user_read.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/user_read.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
+      '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/confirmation_modal.css">',
       '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>',
@@ -106,11 +104,10 @@
 
     case 'media_library.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/media_library.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
+      '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/confirmation_modal.css">',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>',
       '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>',
@@ -119,33 +116,28 @@
 
     case 'media_upload.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/media_upload.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>';
       break;
 
     case 'media_update.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/media_update.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>';
       break;
 
     case 'projects_read.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/projects_read.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
+      '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/confirmation_modal.css">',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>',
       '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>',
@@ -154,33 +146,28 @@
 
     case 'project_create.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/project_create.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>';
       break;
 
     case 'project_update.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
-      '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/projects_update.css">',
+      '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/project_update.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>';
       break;
 
     case 'contacts_read.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/contacts_read.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src=" ' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
+      '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/confirmation_modal.css">',
       '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>',
@@ -190,11 +177,9 @@
 
     case 'contact_update.php':
       echo
-      '<link rel="stylesheet" href="' . BASE_URL . 'css/reset.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'css/back_to_top_btn.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/contacts_update.css">',
       '<link rel="stylesheet" href="' . BASE_URL . 'admin/css/cms_nav.css">',
-      '<script src="' . BASE_URL . 'js/favicon_match_media.js" defer></script>',
       '<script src="' . BASE_URL . 'js/back_to_top_btn.js" defer></script>',
       '<script src="' . BASE_URL . 'admin/js/cms_nav.js" defer></script>';
       break;
