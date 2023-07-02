0798f1da1fc90083c9f105b94c764cfdc91929b9
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-07-02 16:09:44 +0200

    Remove unused links and load name dynamically

--- contact_reaction.php
+++ contact_reaction.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__ . '/includes/head_data.php');
-require_once(__DIR__ . '/includes/nav_data.php');
+require_once(__DIR__ . '/configuration.php');
+
 include(__DIR__ . '/includes/head.inc.php');
 ?>
 
@@ -11,7 +11,7 @@ include(__DIR__ . '/includes/head.inc.php');
   </header>
   <main id="main">
     <h1>Thank you for contacting us.</h1>
-    <p class="text">Dear [name], <br><br> thank you for contacting us. We'll get back to you as soon as possible.
+    <p class="text">Dear <?= $_GET['id'] ?>, <br><br> thank you for contacting us. We'll get back to you as soon as possible.
       <br><br> Your AVD team
     </p>
     <a href="index.php" class="back-to-home">>Back to home</a>
