7ff90a132f2d84e2578704b1e98fd5f036f94c4f
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-07-05 08:28:27 +0200

    Add back btn and remove logo input (media upload)

--- admin/projects/project_create.php
+++ admin/projects/project_create.php
@@ -1,20 +1,13 @@
 <?php
 require_once(dirname(__DIR__) . '/Controller/Project.php');
-// require_once(dirname(dirname(__DIR__)) . '/configuration.php');
 $uploadValidation = new Project();
 $Response = [];
 
-if (isset($_POST) && count($_POST) > 0 && isset($_FILES)) {
-  $Response = $uploadValidation->createProject($_POST, $_FILES);
+if (isset($_POST) && count($_POST) > 0) {
+  $Response = $uploadValidation->createProject($_POST);
 }
 
-// foreach ($uploadValidation->errorsArr as $error) {
-//   $Response['error'] .= $error . "<br>";
-// }
-
-require_once(dirname(__DIR__) . '/includes/admin_head_data.php');
 require_once(dirname(__DIR__) . '/includes/admin_head.inc.php');
-require_once(dirname(__DIR__) . '/includes/cms/nav_data.php');
 
 ?>
 
@@ -22,10 +15,13 @@ require_once(dirname(__DIR__) . '/includes/cms/nav_data.php');
   <?php include(dirname(__DIR__) . '/includes/cms/navigation.inc.php'); ?>
   <main>
     <section>
-      <h2>Add new project:</h2>
+      <div class="flex-container-title">
+        <h2>Add new project:</h2>
+        <a href="projects_read.php" class="go-back">Back to overview &#11152;</a>
+      </div>
       <hr class="title-separator">
-      <form action="" method="POST" enctype="multipart/form-data">
-        <input type="hidden" name="MAX_FILE_SIZE" value="<?= $uploadValidation->maxFileSize ?>">
+      <form action="" method="POST">
+        <!-- <input type="hidden" name="token" value="<?= $_SESSION['token']; ?>"> -->
         <div class="input-container">
           <label for="title">Project title:</label>
           <input type="text" name="title" id="title" value="<?= (isset($_POST['title'])) ? $_POST['title'] : '' ?>">
@@ -35,7 +31,7 @@ require_once(dirname(__DIR__) . '/includes/cms/nav_data.php');
         </div>
         <div class="input-container">
           <label for="year">Project year:</label>
-          <input name="year" type="number" id="year" value="<?= (isset($_POST['year'])) ? $_POST['year'] : '' ?>">
+          <input name="year" type="number" id="year" placeholder="YYYY" value="<?= (isset($_POST['year'])) ? $_POST['year'] : '' ?>">
           <?php if (isset($Response['year']) && !empty($Response['year'])) : ?>
             <span class="error-span"><?= $Response['year']; ?></span>
           <?php endif; ?>
@@ -47,32 +43,18 @@ require_once(dirname(__DIR__) . '/includes/cms/nav_data.php');
             <span class="error-span"><?= $Response['url']; ?></span>
           <?php endif; ?>
         </div>
-        <div class="input-container">
-          <label for="image">Upload company logo:</label>
-          <!-- <input type="file" accept="image/*" name="image" id="image"> -->
-          <!-- for testing -->
-          <input type="file" name="image" id="image">
-          <?php if (isset($Response['error']) && !empty($Response['error'])) : ?>
-            <span class="error-span"><?= $Response['error']; ?></span>
-          <?php endif; ?>
-          <?php if (isset($Response['image']) && !empty($Response['image'])) : ?>
-            <span class="error-span"><?= $Response['image']; ?></span>
-          <?php endif; ?>
-        </div>
         <div class="input-container status-container">
           <label for="status">Status project completed:</label>
-          <input type="checkbox" id="status" name="status" aria-label="project completed" value="completed">
+          <input type="checkbox" id="status" name="status" aria-label="project completed" value="completed" <?= (isset($_POST['status']) && $_POST['status'] == "completed") ? "checked" : '' ?>>
           <?php if (isset($Response['']) && !empty($Response[''])) : ?>
             <span class="error-span"><?= $Response['']; ?></span>
           <?php endif; ?>
         </div>
 
         <button type="submit" name="create" class="create-btn">Create</button>
-        <?php //if (isset($Response['status'])) : 
-        ?>
-        <span class="error-span" class="error-span"><? ?></span>
-        <? //endif; 
-        ?>
+        <?php if (isset($Response['status']) && !$Response['status']) :  ?>
+          <span class="error-span"><?= $Response['status'] ?></span>
+        <?php endif; ?>
       </form>
     </section>
 </body>
