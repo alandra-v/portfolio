e76de9c26a8ee0209bc41f8aaba9c8cd4cb51f72
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-07-10 14:13:41 +0200

    Remove and add configurations
    Remove environment config and add base URL and image upload config

--- configuration.php
+++ configuration.php
@@ -1,14 +1,9 @@
 <?php
-// -------------------------
-// environment configuration
-// -------------------------
+// --------------------------
+// base URL configuration
+// --------------------------
 
-$locale = 'de_DE';
-$timezone = 'CET';
-$dateType = IntlDateFormatter::FULL;
-$timeType = IntlDateFormatter::FULL;
-$calendar = IntlDateFormatter::GREGORIAN;
-
+define('BASE_URL', 'http://localhost:8888/portfolio_avd/');
 // ----------------------
 // session configuration
 // ----------------------
@@ -20,7 +15,18 @@ define('CONFIG_SESSION_LIFETIME', 1800);
 // database configuration
 // ----------------------
 
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root'); // when using XAMPP leaver empty '' when using MAMP enter 'root'
-define('DB_NAME', 'portfolio_admin');
+define('DB_NAME', 'portfolio_admin'); // Database name
+define('DB_SERVER', 'localhost'); // Database host
+define('DB_USER', 'root'); // Database root
+define('DB_PASSWORD', 'root'); // Database password
+
+// --------------------------
+// image upload configuration
+// --------------------------
+
+define('TARGET_DIR', '../../assets/images/uploads'); // Target directory
+// WEBP will be allowed as soon as multiple image upload is possible to support picture tag
+define('ALLOWED_MIME_TYPES', ['image/jpeg', 'image/png', 'image/svg']); // Allowed MIME types
+// WEBP will be allowed as soon as multiple image upload is possible to support picture tag
+define('ALLOWED_EXTENSIONS', ['png', 'jpg', 'jpeg', 'svg']); // Allowed file extensions
+define('MAX_FILE_SIZE', 3000000);  // Max. allowed file size
