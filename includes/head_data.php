92a3b96edaa49c5af8ccfa5ea88401164fa9fc5d
Alandra Villalaz <alandra.villalaz@gmail.com>
2023-07-11 07:25:31 +0200

    Rename project_aebele_interiors.php to project.php
    Since the project page is dynamic now

--- includes/head_data.php
+++ includes/head_data.php
@@ -9,8 +9,7 @@ $pageTitle = array(
   'index.php' => 'Home',
   'legal_disclosure.php' => 'Legal Disclosure',
   'privacy_policy.php' => 'Privacy Policy',
-  // project dynamic titles?
-  'project_aebele_interiors.php' => 'Work - Project',
+  'project.php' => 'Work - Project',
   'services.php' => 'Services',
   'work.php' => 'Work',
 );
@@ -20,10 +19,9 @@ $metaDescription = array(
   'contact_reaction.php' => 'Thank you for contacting us. We\'ll get back to you as soon as possible. Your AVD team',
   'contact.php' => 'We\'re looking forward to hearing about your project. Contact us now or schedule a meeting.',
   'index.php' => 'A unique partner for unique projects. From the concept to the finished web project tailored to your and your customers needs.',
-  'legal_disclosure.php' => 'Legal disclosure. AV Development AG Via Lieptgas 3, 7018 Flims-Waldhaus, E-mail: alandra.villalaz@gmail.com',
+  'legal_disclosure.php' => 'Legal disclosure. AV Development AG 7018 Flims-Waldhaus, E-mail: alandra.villalaz@gmail.com',
   'privacy_policy.php' => 'This Privacy Policy will inform you about the types of personal data we collect, the purposes of the data, the ways in which it is handled and your rights.',
-  // project dynamic titles?
-  'project_aebele_interiors.php' => 'Æbele Interiors offers a full range of bespoke interior design services. They commissioned us to create a website that reflects their corporate personality.',
+  'project.php' => 'Dive into detail of a collection of meticulously crafted projects that demonstrate my expertise in front-end and back-end development, responsive design and user experience optimization.',
   'services.php' => 'We offer a variety of different services before/during/after the go-live of your web project ranging from design to technical implementation.',
   'work.php' => 'Everything at glance - projects and customers',
 );
